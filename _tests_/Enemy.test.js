4826068752c2708b00be51aa95267654a010fdaa
smardone <shaun.mardones@gmail.com>
2020-08-05 22:37:41 -0400

    created Enemy constructor and tests

---
+++ _tests_/Enemy.test.js
@@ -0,0 +1,17 @@
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+
+const Enemy = require('../lib/Enemy');
+
+
+test('creates an enemy object', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.name).toBe('goblin');
+    expect(enemy.weapon).toBe('sword');
+    expect(enemy.health).toEqual(expect.any(Number));
+    expect(enemy.strength).toEqual(expect.any(Number));
+    expect(enemy.agility).toEqual(expect.any(Number));
+    expect(enemy.potion).toEqual(expect.any(Object));
+});
